fc7396a81a8bf16d9083a9536753286b9b27c3ab
Salvador <salvador.reynoso@fau.de>
2022-09-08 18:01:19 +0200

    Playing with references

--- src/python_implementation/config.py
+++ src/python_implementation/config.py
@@ -2,7 +2,7 @@
 
 # data_path = "../../data/TCGA-COAD.htseq_fpkm.tsv"
 # data_path = "/genie3/data.txt"
-data_path = "~/GenesFRFFinal/data/TCGA-COAD.htseq_fpkm.tsv"
+data_path = "/home/hpc/iwbn/iwbn001h/GenesFRFFinal/data/TCGA-COAD.htseq_fpkm.tsv"
 path_transcription_factors = '~/GenesFRFFinal/data/Regulators.txt'
 path_to_genie3_R = '~/GenesFRFFinal/genie3/Genie3.R'
 data_path_to_VIM_matrices = "~/GenesFRFFinal/data/"
