9109f3af182a0bb07a70fff451cf41bd35083c49
Salvador <salvador.reynoso@fau.de>
2022-09-08 17:22:56 +0200

    Playing with references

--- src/python_implementation/config.py
+++ src/python_implementation/config.py
@@ -2,10 +2,10 @@
 
 # data_path = "../../data/TCGA-COAD.htseq_fpkm.tsv"
 # data_path = "/genie3/data.txt"
-data_path = "/media/sf_Projekt_BIONETS/federated-inference-of-grns/data/TCGA-COAD.htseq_fpkm.tsv"
-path_transcription_factors = '/media/sf_Projekt_BIONETS/federated-inference-of-grns/data/Regulators.txt'
-path_to_genie3_R = '/media/sf_Projekt_BIONETS/federated-inference-of-grns/genie3/Genie3.R'
-data_path_to_VIM_matrices = "/media/sf_Projekt_BIONETS/federated-inference-of-grns/data/"
+data_path = "~/GenesFRFFinal/data/TCGA-COAD.htseq_fpkm.tsv"
+path_transcription_factors = '~/GenesFRFFinal/data/Regulators.txt'
+path_to_genie3_R = '~/GenesFRFFinal/genie3/Genie3.R'
+data_path_to_VIM_matrices = "~/GenesFRFFinal/data/"
 
 number_trees = 500
 tree_method = "RF"
