34789e2ebb4d9a043f122c51bc4b102f3497a9fb
Salvador <salvador.reynoso@fau.de>
2022-09-21 12:02:41 +0100

    Added Batch file

--- src/python_implementation/config.py
+++ src/python_implementation/config.py
@@ -8,10 +8,10 @@ path_to_genie3_R = '/data_slow/xo53tota/GenesFRFFinal/genie3/Genie3.R'
 data_path_to_VIM_matrices = "/data_slow/xo53tota/GenesFRFFinal/data/"
 path_to_results = '/data_slow/xo53tota/GenesFRFFinal/results/'
 
-number_trees = 5000
+number_trees = 500
 tree_method = "RF"
 
-number_of_hospitals = 3
+number_of_hospitals = 10
 split_even = True
 # is the list of how to split uneven data: len must be of number_of_hospitals
 split_uneven = [0.1, 0.2, 0.7]
