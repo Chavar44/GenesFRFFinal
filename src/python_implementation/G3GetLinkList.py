dca62d1d10b5eb24d39da05733a4e6a51e214f02
Salvador <salvador.reynoso@fau.de>
2022-09-23 19:36:59 +0100

    Added genie3 linklist calculation program

---
+++ src/python_implementation/G3GetLinkList.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 23 10:38:09 2022
+
+@author: chvr4
+"""
+import sys
+sys.path.insert(0,"/data_slow/xo53tota/GenesFRFFinal")
+from genie3.GENIE3 import *
+#from ~/GenesFRFFinal/genie3/GENIE3 import *
+from src.python_implementation.main import *
+from sklearn.metrics import mean_squared_error
+import numpy as np
+from matplotlib import pyplot as plt
+import time
+import logging
+import subprocess
+import src.python_implementation.config as config
+import os
+from numpy import unravel_index
+
+max_count_link_list = 500000
+
+
+
+#import regulator names
+path_tf=config.path_transcription_factors
+tf = np.loadtxt(path_tf, dtype=str)
+tf = tf.tolist()
+print("Regulators read")
+
+#import TCGA data and read gene_names
+path = config.data_path
+data = np.loadtxt(path, dtype=str, skiprows=1)
+raw_gene_names = data[:, :1]
+# Transform raw_gene_names into a list readable by the federated random forest approach
+gene_names = []
+for xs in raw_gene_names:
+    for x in xs:
+        # Only reads the first 15 characters in order to correctly compare to the Regulators.txt file
+        gene_names.append(x[0:15])
+print("Gene names read")    
+
+#Get order of regulators in matrix
+Regulators = []
+j=0
+for i in gene_names:
+    for j in tf:
+        if i == j:
+            Regulators.append(j)
+
+print("Regulators in order") 
+
+
+#import G3 VIM matrix
+path = config.data_path_to_VIM_matrices + "Weight_Matrix.npy"
+G3 = np.load(path).astype(float)
+print("Finished reading G3 Matrix")
+
+with open('G3_linked_list_500000.txt', 'w') as f:
+    
+    for i in range(0,max_count_link_list):
+        coords = unravel_index(G3.argmax(), G3.shape) #get coordinates for the VIM Matrix in order to know the exact gene
+        line =  Regulators[coords[0]]  + " " + gene_names[coords[1]] + " " + str(G3[coords[0]][coords[1]])
+        f.write(line)
+        f.write('\n')
+        print(str(i) + " " + line)
+        G3[coords[0]][coords[1]] = 0
+    
+f.close()
+
+
+
