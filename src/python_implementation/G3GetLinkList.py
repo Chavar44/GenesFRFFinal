e812e0baf251119b02cba0c4a0d3991c36ee77fc
Salvador <salvador.reynoso@fau.de>
2022-09-24 10:37:31 +0100

    Changed calculation number

--- src/python_implementation/G3GetLinkList.py
+++ src/python_implementation/G3GetLinkList.py
@@ -19,7 +19,7 @@ import src.python_implementation.config as config
 import os
 from numpy import unravel_index
 
-max_count_link_list = 1000000
+max_count_link_list = 1500000
 
 
 
@@ -53,11 +53,11 @@ print("Regulators in order")
 
 
 #import G3 VIM matrix
-path = config.data_path_to_VIM_matrices + "VIM_federated_3_even.npy"
+path = config.data_path_to_VIM_matrices + "Weight_Matrix.npy"
 G3 = np.load(path).astype(float)
 print("Finished reading G3 Matrix")
 
-with open('F_linked_list_1000000.txt', 'w') as f:
+with open('G3_linked_list_1500000.txt', 'w') as f:
     
     for i in range(0,max_count_link_list):
         coords = unravel_index(G3.argmax(), G3.shape) #get coordinates for the VIM Matrix in order to know the exact gene
