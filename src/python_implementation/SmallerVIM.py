87af59418fc26c2b4850b5c4c76dd20ada55adc1
Salvador <salvador.reynoso@fau.de>
2022-09-25 11:10:44 +0100

    Added Federated VIM Conversion algorithm to remove unnecesary genes

--- src/python_implementation/SmallerVIM.py
+++ src/python_implementation/SmallerVIM.py
@@ -21,7 +21,7 @@ from numpy import unravel_index
 path = config.data_path_to_VIM_matrices + "VIM_federated_3_even.npy"
 BigVIM = np.load(path).astype(float)
 print("Finished reading Big VIM federated Matrix")
-SmallVIM = np.zeros((1636, 60483))
+SmallVIM = np.zeros((1637, 60483))
 
 #import regulator names
 path_tf=config.path_transcription_factors
