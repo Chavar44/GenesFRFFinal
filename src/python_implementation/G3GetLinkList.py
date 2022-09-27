d4df3204feb29a411abad746f63e29b97f801972
Salvador <salvador.reynoso@fau.de>
2022-09-27 23:30:22 +0100

    Update getlinklist for uneven3 VIM

--- src/python_implementation/G3GetLinkList.py
+++ src/python_implementation/G3GetLinkList.py
@@ -19,7 +19,7 @@ import src.python_implementation.config as config
 
 from numpy import unravel_index
 
-max_count_link_list = 1500000
+max_count_link_list = 1000000
 
 
 
@@ -53,11 +53,11 @@ print("Regulators in order")
 
 
 #import G3 VIM matrix
-path = config.data_path_to_VIM_matrices + "FVIM_small_even_3.npy"
+path = config.data_path_to_VIM_matrices + "VIM_federated_3_uneven.npy"
 G3 = np.load(path).astype(float)
 print("Finished reading F Matrix")
 
-with open('F_even_3_linked_list_1500000.txt', 'w') as f:
+with open('F_uneven_3_linked_list_1000000.txt', 'w') as f:
     
     for i in range(0,max_count_link_list):
         coords = unravel_index(G3.argmax(), G3.shape) #get coordinates for the VIM Matrix in order to know the exact gene
