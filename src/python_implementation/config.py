7a5b1d14271f069b3559876cb729903bd075a8a1
Salvador <salvador.reynoso@fau.de>
2022-10-01 20:34:09 +0100

    Changed config for fast evaluation purposes

--- src/python_implementation/config.py
+++ src/python_implementation/config.py
@@ -11,10 +11,10 @@ path_to_results = '/data_slow/xo53tota/GenesFRFFinal/results/'
 number_trees = 500
 tree_method = "RF"
 
-number_of_hospitals = 10
-split_even = True
+number_of_hospitals = 3
+split_even = False
 # is the list of how to split uneven data: len must be of number_of_hospitals
-split_uneven = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
+split_uneven = [0.1, 0.2, 0.7]
 #For reminder purposes split_uneven = [0.1, 0.075, 0.075, 0.225, 0.125, 0.075, 0.075, 0.1, 0.075, 0.075]
 #For reminder purposes split_uneven = [0.1, 0.2, 0.7]
 
