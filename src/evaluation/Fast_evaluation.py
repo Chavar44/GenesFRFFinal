72fac2a2ec5d2e7a1a15ae2b16839e5eecd7a7f0
Salvador <salvador.reynoso@fau.de>
2022-10-02 22:21:33 +0100

    changed linklist for 3 uneven

--- src/evaluation/Fast_evaluation.py
+++ src/evaluation/Fast_evaluation.py
@@ -29,7 +29,7 @@ if config.split_even:
     split_name = 'even'
 
 logger.info('loading link list from Federated Approach')
-path = os.path.join(config.data_path_to_VIM_matrices, "Federated_3_uneven_linked_list_5000000.txt")
+path = os.path.join(config.data_path_to_VIM_matrices, "Federated_3_even_linked_list_5000000.txt")
 edges_federated = np.loadtxt(path, dtype=str, delimiter=" ")
 
 logger.info('loading link list from Genie3')
